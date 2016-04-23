760ef978f8a6af38acc9fc331618166a0c32b970
afsanchezs <afsanchezs96@gmail.com>
2016-04-22 23:22:15 -0500

    Simulacion y prueba L293D giro de motores

---
+++ Prueba_giro_motor/Prueba_giro_motor..ino
@@ -0,0 +1,15 @@
+void setup() {
+ pinMode(0,OUTPUT);
+ pinMode(1,OUTPUT);
+ pinMode(2,OUTPUT);
+ pinMode(3,OUTPUT);   
+
+}
+
+void loop() {
+  digitalWrite(0,LOW);
+  digitalWrite(1,HIGH);
+  digitalWrite(2,HIGH);
+  digitalWrite(3,LOW);
+
+}
