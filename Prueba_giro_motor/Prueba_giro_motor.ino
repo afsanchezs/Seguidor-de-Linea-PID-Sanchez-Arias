54de2504d6f1133f0588411cd0433b2933709bd6
afsanchezs <afsanchezs96@gmail.com>
2016-04-29 23:49:48 -0500

    Se ingresan las entradas y salidas del sistema

--- Prueba_giro_motor/Prueba_giro_motor.ino
+++ Prueba_giro_motor/Prueba_giro_motor.ino
@@ -1,15 +1,33 @@
 void setup() {
- pinMode(0,OUTPUT);
- pinMode(1,OUTPUT);
- pinMode(2,OUTPUT);
- pinMode(3,OUTPUT);   
-
+  // put your setup code here, to run once:
+  pinMode(10,OUTPUT);//Salida PID PWM Motor 2
+  pinMode(9,OUTPUT);//Salida PID PWM Motor 1
+  pinMode(7,INPUT);//4-7 Sensores IR
+  pinMode(6,INPUT);
+  pinMode(5,INPUT);
+  pinMode(4,INPUT);
+  pinMode(3,OUTPUT);//2 y 3 señales de control motor 2
+  pinMode(2,OUTPUT);
+  pinMode(1,OUTPUT);//1 y 0 señales de control motor 1
+  pinMode(0,OUTPUT);
+  pinMode(A0,INPUT);//Kp
+  pinMode(A1,INPUT);//Kd
+  pinMode(A2,INPUT);//Ki
 }
 
 void loop() {
-  digitalWrite(0,LOW);
-  digitalWrite(1,HIGH);
-  digitalWrite(2,HIGH);
-  digitalWrite(3,LOW);
-
-}
+  // put your main code here, to run repeatedly:
+  // Hacer detener los motores sería un gasto innecesario
+  // de batería, por lo tanto, siempre giraran en el mismo
+  // sentido. 
+  //En línea recta. A máxima velocidad 
+  if((digitalRead(5)==HIGH)&(digitalRead(6)==HIGH))
+  {
+    digitalWrite(0,HIGH);
+    digitalWrite(1,LOW);
+    digitalWrite(2,LOW);
+    digitalWrite(3,HIGH);
+    analogWrite(9,255);//El PWM será controlado por el PID
+    analogWrite(10,255);
+  }
+  //
